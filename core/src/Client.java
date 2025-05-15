58a0f696dc4a6bf87f9d55d3cfa56766898f3597
Roman Skvortsov <79626105+SayM0Re@users.noreply.github.com>
2025-05-15 23:20:42 +0300

    feat: divided classes into files
    divided classes into files for Maven assembly

---
+++ core/src/Client.java
@@ -0,0 +1,18 @@
+
+public class Client {
+    private String id;
+    private String name;
+    
+    public Client(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public String getId() { return id; }
+    public String getName() { return name; }
+    
+    @Override
+    public String toString() {
+        return "\nClient: " + name + " (ID: " + id + ")";
+    }
+}
---
+++ core/src/Transaction.java
@@ -0,0 +1,36 @@
+import java.util.Date;
+
+class Transaction {
+    public enum Type {
+        DEPOSIT, WITHDRAW
+    }
+
+    private Type type;
+    private double amount;
+    private Date date;
+    private Account account;
+
+    public Transaction(Type type, double amount, Account account) {
+        this.type = type;
+        this.amount = amount;
+        this.account = account;
+        this.date = new Date();
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    @Override
+    public String toString() {
+        return date + " | " + type + " | Amount: $" + amount + " | Account: " + account.getAccountNumber();
+    }
+}
